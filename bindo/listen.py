3b29363ec3191a05f06b035af1219084f4040c6b
Viktor <pennine@protonmail.com>
2020-02-25 00:08:23 +0300

    Basic message handling on listening port.

--- bindo/listen.py
+++ bindo/listen.py
@@ -5,7 +5,9 @@ import threading
 class Listen(threading.Thread):
 
     def __init__(self, port: int) -> None:
+        self.buffer = bytes()
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((socket.gethostname(), port))
         self.server.listen(1)
         super().__init__()
@@ -17,3 +19,13 @@ class Listen(threading.Thread):
             # Do something with this socket.
             # My guess is that we can recieve message such as "PeerInit" and
             # "PierceFirewall" from this socket.
+            data = socket.recv(4096)
+            if not data:
+                continue
+
+            # self.buffer += data
+            self.buffer += data
+            message_len = int.from_bytes(self.buffer[:4], 'little')
+            (message_len,) = struct.unpack('<i', self.buffer[:4])
+            if len(self.buffer) >= message_len:
+                message_code = int.from_bytes(self.buffer[4:5], 'little')
