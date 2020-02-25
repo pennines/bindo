6d1e270bdf287f7d2b320ff3ad1cdcf7c0e5c51c
Viktor <pennine@protonmail.com>
2020-02-25 17:54:21 +0300

    Send an incoming connection via callback instead of dealing with it in the thread.

--- bindo/listen.py
+++ bindo/listen.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-from typing import Callable, Dict, Union
+from typing import Callable, Dict, Type, Tuple, Union
 
 from .message import PeerInitMessage
 
@@ -8,8 +8,7 @@ from .message import PeerInitMessage
 class Listen(threading.Thread):
 
     def __init__(self, port: int,
-                 callback: Callable[[Dict[str, Union[str, int]]], None]) -> None:
-        self.buffer = bytes()
+                 callback: Callable[[Type[socket.socket], Tuple[str, int]], None]) -> None:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((socket.gethostname(), port))
@@ -20,19 +19,7 @@ class Listen(threading.Thread):
 
     def run(self) -> None:
         while True:
+            # Listen for incoming connections, send an incoming connection via
+            # callback.
             (socket, address) = self.server.accept()
-            # Do something with this socket.
-            # My guess is that we can recieve message such as "PeerInit" and
-            # "PierceFirewall" from this socket.
-            data = socket.recv(4096)
-            if not data:
-                continue
-
-            # self.buffer += data
-            self.buffer += data
-            message_len = int.from_bytes(self.buffer[:4], 'little')
-            if len(self.buffer) >= message_len:
-                message_code = int.from_bytes(self.buffer[4:5], 'little')
-                message = PeerInitMessage.unpack_message(message_code, self.buffer)
-                self.callback(message)
-                self.buffer = self.buffer[message_len + 1:]
+            self.callback(socket, address)
