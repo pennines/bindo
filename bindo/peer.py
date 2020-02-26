4bda9a260a43aa32709b255719156e58d5df8fce
Viktor <pennine@protonmail.com>
2020-02-26 21:56:11 +0300

    Move a peer connection to a seperate thread.

---
+++ bindo/peer.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+from typing import Type, Tuple, Callable, Union, Dict
+
+from .message import PeerMessage
+
+
+class Peer(threading.Thread):
+
+    def __init__(self, socket: Type[socket.socket],
+                 callback: Callable[[Dict[str, Union[str, int]]], None]) -> None:
+        self.buffer = bytes()
+        self.callback = callback
+        self.connection = socket
+        super().__init__()
+        self.daemon = True
+
+    def run(self) -> None:
+        while True:
+            data = self.connection.recv(4096)
+
+            self.buffer += data
+            message_len = int.from_bytes(self.buffer[:4], 'little')
+            if len(self.buffer) >= message_len:
+                # TODO: There two types of messages that we can recieve from this socket.
+                #   PeerMessage,
+                #   PeerInitMessage,
+                #   PeerMessage uses 4 bytes to indicate message code.
+                #   PeerInitMessage uses 1 byte to indicate message code.
+                #   HOW TO HANDLE THIS?
+                message_code = int.from_bytes(self.buffer[4:5], 'little')
+                message = PeerMessage.unpack_message(message_code, self.buffer)
+                print(message_len, message_code)
+                self.callback(message)
+                self.buffer = self.buffer[message_len + 4:]
